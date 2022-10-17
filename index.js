897441202673c1622dfcea3f305e716059f99c30
yen-jung <52653240+yen-jung@users.noreply.github.com>
2022-10-17 17:12:51 +0800

    123

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello Node.js'); // 在terminal 印出 Hello Node.js
+// index.js
+
+//隨便一個函數
+function isPrime(num) {
+    for (var i = 2; i <= Math.sqrt(num); i++)
+      if (num % i === 0) return false;
+    return num > 1;
+  }
+  //記得輸出
+  module.exports = isPrim
